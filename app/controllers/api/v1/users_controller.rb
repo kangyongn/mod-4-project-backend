1e8e1a06e4a712ef64ee1a7c706dd0c8a7556104
github username <github email address>
2019-04-15 10:03:34 -0400

    added jwt gem, password_confirmation to user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -36,7 +36,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:email, :password, :first_name, :last_name)
+    params.permit(:email, :password, :password_confirmation, :first_name, :last_name)
   end
 
   def get_user
