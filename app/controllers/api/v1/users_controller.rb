d916e2c5147f7ddad42f119d8eebce6f1331902c
github username <github email address>
2019-04-15 11:56:14 -0400

    fixed signup logic to stay logged in after signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,7 +13,8 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created
+      @token = JWT.encode({user_id: @user.id}, "secret")
+      render json: { user: @user, jwt: @token }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
