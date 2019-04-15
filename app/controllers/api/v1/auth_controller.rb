f747b965a0777c844bca434a4905682588883df7
github username <github email address>
2019-04-15 15:19:05 -0400

    updated auth_controller to send user.gifts

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::AuthController < ApplicationController
     @user = User.find_by(email: login_params[:email])
     if @user && @user.authenticate(login_params[:password])
       token = JWT.encode({ user_id: @user.id }, "secret")
-      render json: { user:@user , jwt: token }, status: :accepted
+      render json: { user:@user, gifts:@user.gifts , jwt: token }, status: :accepted
     else
       render json: { message: 'Invalid username or password' }, status: :unauthorized
     end
